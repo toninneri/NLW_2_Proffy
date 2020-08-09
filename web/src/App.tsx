763b95e33bcbc0fbeef6175cb260e1cf00513a28
toninneri <toninneri@gmail.com>
2020-08-09 20:19:30 -0300

    Start login functionality

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,7 +3,18 @@ import React, { ReactElement } from 'react';
 import Routes from './routes';
 
 import './assets/styles/global.css';
+//Added Login functionality
+import HomePage from './components/HomePage/HomePage';
+import LoginPage from './components/LoginPage/LoginPage';
 
+const Home = () => (
+  <HomePage />
+);
+
+const Login = () => (
+  <LoginPage />
+);
+//till here
 function App(): ReactElement {
   return <Routes />;
 }
